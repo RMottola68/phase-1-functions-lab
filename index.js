1cc6e1592f5795392cbe63780cf5d2a55c6ae72b
Ryan Mottola <rmottola68@gmail.com>
2022-06-06 17:28:48 -0400

    commit tree 3d50e8f8d37f1cbadcab69adecd56900d2580084

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+let distanceFromHqInBlocks = (pickUp) => {
+    let distance = Math.abs(42 - pickUp);
+    return distance;
+}
+
+let distanceFromHqInFeet = (pickUp) => {
+    let distanceInFeet = (Math.abs(42-pickUp) * 264);
+    return distanceInFeet;
+}
+
+let distanceTravelledInFeet = (num1, num2) => {
+ return (Math.abs(num1 - num2)) * 264;
+}
+
+let calculatesFarePrice = (start, destination) => {
+    
+    let farePrice = 0;
+    let feetTravelled = distanceTravelledInFeet(start, destination)
+    if( feetTravelled < 400) {
+        return farePrice;
+    } else if((feetTravelled >= 400) && (feetTravelled < 2000)){
+        farePrice = (feetTravelled -400) * .02;
+        return farePrice;
+    } else if(feetTravelled > 2000 && feetTravelled <= 2500) {
+        farePrice = 25
+        return farePrice;
+    } else if(feetTravelled > 2500) {
+        farePrice =  "cannot travel that far";
+        return farePrice;
+    }
+}
